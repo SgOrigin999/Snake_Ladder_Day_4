fea7cf364d0924a93fa1484a99d2ac035918d590
SgOrigin999 <you@example.com>
2021-05-31 16:08:12 +0530

    Roll Dice to get Numer between 1 to 6

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,5 +1,7 @@
 package com.bridgelab.snakeladder.day4;
 
+import java.util.Random;
+
 public class SnakeLadder {
 	
 	public static int singlePlayeWithZeroPosition() {
@@ -9,9 +11,19 @@ public class SnakeLadder {
 		return player;
 	}
 	
+	public int rollDice() {
+		int n = 0;
+		Random r = new Random();
+		n = r.nextInt(7);
+		System.out.println("DICEVALUE::" + n);
+		return (n == 0 ? 1 : n);
+	}
+	
 public static void main(String[] args) {
+	SnakeLadder snake= new SnakeLadder();
 	System.out.println("Snake Ladder Game Play With Single Player::");
 	int singlePlaye=singlePlayeWithZeroPosition();
-	
+	System.out.println("Player Rool The die to get Number 1 to 6 Range only::");
+	int n=snake.rollDice();
 }
 }
