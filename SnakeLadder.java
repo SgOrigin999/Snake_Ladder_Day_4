149ff8a5cf40b66d8293925e962b5b89e0a49162
SgOrigin999 <you@example.com>
2021-05-31 16:00:30 +0530

    Single Player Start with 0 Position

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -2,7 +2,16 @@ package com.bridgelab.snakeladder.day4;
 
 public class SnakeLadder {
 	
+	public static int singlePlayeWithZeroPosition() {
+		int player;
+		int playerPosition = 0;
+		player=playerPosition; 
+		return player;
+	}
+	
 public static void main(String[] args) {
 	System.out.println("Snake Ladder Game Play With Single Player::");
+	int singlePlaye=singlePlayeWithZeroPosition();
+	
 }
 }
