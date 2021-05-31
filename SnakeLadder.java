86f566731fa822f3d25d8410447b7696d42ac010
SgOrigin999 <you@example.com>
2021-05-31 15:40:27 +0530

    Single player with Position o

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -3,6 +3,6 @@ package com.bridgelab.snakeladder.day4;
 public class SnakeLadder {
 	
 public static void main(String[] args) {
-	
+	System.out.println("Snake Ladder Game Play With Single Player::");
 }
 }
