ffcbae9b331ee0f029c9babfd390bf80d2865877
SgOrigin999 <you@example.com>
2021-05-31 17:04:09 +0530

    Repeat till player reach Wining Position 100

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,9 +1,31 @@
 package com.bridgelab.snakeladder.day4;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
+import java.util.Scanner;
 
 public class SnakeLadder {
+	final static int WINPOINT = 100;
+	static int GETLADDER = 0;
+	
+	static Map<Integer, Integer> snake = new HashMap<Integer, Integer>();
+	static Map<Integer, Integer> ladder = new HashMap<Integer, Integer>();
 
+	{
+		snake.put(99, 54);
+		snake.put(70, 55);
+		snake.put(52, 42);
+		snake.put(25, 2);
+		snake.put(95, 72);
+
+		ladder.put(6, 25);
+		ladder.put(11, 40);
+		ladder.put(60, 85);
+		ladder.put(46, 90);
+		ladder.put(17, 69);
+	}
+	
 	public static int singlePlayeWithZeroPosition() {
 		int player;
 		int playerPosition = 0;
@@ -32,6 +54,53 @@ public class SnakeLadder {
 		}
 		return n;
 	}
+	
+	public boolean playerWin() {
+		Scanner sc=new Scanner(System.in);
+		String str;
+		int diceValue;
+		boolean result = false;
+		int singlePlaye = singlePlayeWithZeroPosition();
+		do {
+			System.out.println("Press r to roll Dice");
+			str = sc.next();
+			diceValue = rollDice();
+			if (singlePlaye <= 100) {
+				singlePlaye = calculatePlayerValue(singlePlaye, diceValue);
+				System.out.println("First Player :: " + singlePlaye);
+				if (isWin(singlePlaye)) {
+					System.out.println("player Wins");
+					result= true;
+				}
+			}
+		} while ("r".equals(str));
+		return result;
+	}
+	
+	public int calculatePlayerValue(int player, int diceValue) {
+		
+		player = player + diceValue;
+
+		if (player > WINPOINT) {
+			player = player - diceValue;
+			return player;
+		}
+
+		if (null != snake.get(player)) {
+			System.out.println("swallowed by snake");
+			player = snake.get(player);
+		}
+
+		if (null != ladder.get(player)) {
+			System.out.println("climb up the ladder");
+			player = ladder.get(player);
+		}
+		return player;
+	}
+
+	public boolean isWin(int player) {
+		return WINPOINT == player;
+	}
 
 	public static void main(String[] args) {
 		SnakeLadder snake = new SnakeLadder();
@@ -41,6 +110,8 @@ public class SnakeLadder {
 		int n = snake.rollDice();
 		System.out.println("Check Player Play Option::");
 		int moveOfPlayer=snake.checkOptionThenPlay();
+		System.out.println("Repeat Till Player Reach Wining Position 100::"+snake.playerWin());
+		
 
 	}
 }
